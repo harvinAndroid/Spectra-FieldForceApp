9e4a70687a8ae5e52c3becad927c219b7012c5f9
satyveeryadav <satyveer.yadav@spectra.co>
2020-07-09 20:56:40 +0530

    RC1, RC2, RC3

---
+++ app/src/main/java/com/spectra/fieldforce/Model/RCRequest.java
@@ -0,0 +1,40 @@
+package com.spectra.fieldforce.Model;
+
+public class RCRequest {
+    String Authkey = "";
+    String Action = "";
+    String RConeId = "";
+    String RCtwoId = "";
+
+    public String getAuthkey() {
+        return Authkey;
+    }
+
+    public void setAuthkey(String authkey) {
+        Authkey = authkey;
+    }
+
+    public String getAction() {
+        return Action;
+    }
+
+    public void setAction(String action) {
+        Action = action;
+    }
+
+    public String getRC1() {
+        return RConeId;
+    }
+
+    public void setRC1(String rc1) {
+        RConeId = rc1;
+    }
+
+    public String getRC2() {
+        return RCtwoId;
+    }
+
+    public void setRC2(String rc2) {
+        RCtwoId = rc2;
+    }
+}
--- app/src/main/java/com/spectra/fieldforce/SRDetail.java
+++ app/src/main/java/com/spectra/fieldforce/SRDetail.java
@@ -1,7 +1,6 @@
 package com.spectra.fieldforce;
 
 import android.Manifest;
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -10,23 +9,24 @@ import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
-
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
-import com.spectra.fieldforce.Model.AssignmentAdapter;
-import com.spectra.fieldforce.Model.AssignmentRequest;
 import com.spectra.fieldforce.Model.Order;
+import com.spectra.fieldforce.Model.RCRequest;
 import com.spectra.fieldforce.Model.SRRequest;
 
 import org.json.JSONArray;
@@ -35,7 +35,6 @@ import org.json.JSONObject;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -54,23 +53,22 @@ public class SRDetail extends Fragment {
             srStatus, srType, srSubType, slaClock, slaStatus, customerIP, segment, devicePort, podName,
             startTime, endTime, startLocation, endLocation;
     public Button btnView, btnStartTime, btnEndTime;
+    public Spinner rc1, rc2, rc3;
     private String status;
     private JSONArray result;
     AppCompatActivity activity;
+    ArrayList<String> rc1Code;
+    ArrayList<String> rc1Name;
+    ArrayList<String> rc2Code;
+    ArrayList<String> rc2Name;
+    ArrayList<String> rc3Code;
+    ArrayList<String> rc3Name;
     private Location location;
 
     public SRDetail() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SRDetail.
-     */
     // TODO: Rename and change types and number of parameters
     public static SRDetail newInstance(String param1, String param2) {
         Log.d("para", param1);
@@ -79,6 +77,172 @@ public class SRDetail extends Fragment {
         args.putString("srNumber", param1);
         fragment.setArguments(args);
         return fragment;
+    }
+
+    private void getRC1() {
+        String authKey = "ac7b51de9d888e1458dd53d8aJAN3ba6f";
+        String action = "getRCone";
+
+        RCRequest rcRequest = new RCRequest();
+        rcRequest.setAuthkey(authKey);
+        rcRequest.setAction(action);
+
+        RCInterface apiService = ApiClient.getClient().create(RCInterface.class);
+        Call<JsonElement> call = apiService.getRCDetail(rcRequest);
+        call.enqueue(new Callback<JsonElement>() {
+            @Override
+            public void onResponse(retrofit2.Call<JsonElement> call, Response<JsonElement> response) {
+                try {
+                    if (response.isSuccessful()) {
+                        JSONObject jsonObject = new JSONObject(String.valueOf(response.body()));
+                        status = jsonObject.getString("Status");
+                        if (status.equals("Failure")) {
+                            Log.d("Failure", "error");
+                        } else if (status.equals("Success")) {
+                            try {
+                                result = jsonObject.getJSONArray("data");
+                                if (result != null) {
+                                    rc1Code = new ArrayList<String>();
+                                    rc1Name = new ArrayList<String>();
+                                    for (int i = 0; i < result.length(); i++) {
+                                        JSONObject jsonData = new JSONObject(String.valueOf(result.getString(i)));
+                                        Log.d("RC1Response", jsonData.toString());
+                                        String code = jsonData.getString("rc_oneId");
+                                        String name = jsonData.getString("rootCauseOne");
+                                        rc1Code.add(code);
+                                        rc1Name.add(name);
+                                    }
+                                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, rc1Name);
+                                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                                    rc1.setAdapter(adapter);
+                                }
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(retrofit2.Call<JsonElement> call, Throwable t) {
+                Log.e("RetroError", t.toString());
+            }
+        });
+    }
+
+    private void getRC2(String rc1Code) {
+        String authKey = "ac7b51de9d888e1458dd53d8aJAN3ba6f";
+        String action = "getRCtwo";
+        String RConeId = rc1Code;
+
+        RCRequest rcRequest = new RCRequest();
+        rcRequest.setAuthkey(authKey);
+        rcRequest.setAction(action);
+        rcRequest.setRC1(RConeId);
+
+        RCInterface apiService = ApiClient.getClient().create(RCInterface.class);
+        Call<JsonElement> call = apiService.getRCDetail(rcRequest);
+        call.enqueue(new Callback<JsonElement>() {
+            @Override
+            public void onResponse(retrofit2.Call<JsonElement> call, Response<JsonElement> response) {
+                try {
+                    if (response.isSuccessful()) {
+                        JSONObject jsonObject = new JSONObject(String.valueOf(response.body()));
+                        status = jsonObject.getString("Status");
+                        if (status.equals("Failure")) {
+                            Log.d("Failure", "error");
+                        } else if (status.equals("Success")) {
+                            try {
+                                result = jsonObject.getJSONArray("data");
+                                if (result != null) {
+                                    rc2Code = new ArrayList<String>();
+                                    rc2Name = new ArrayList<String>();
+                                    for (int i = 0; i < result.length(); i++) {
+                                        JSONObject jsonData = new JSONObject(String.valueOf(result.getString(i)));
+                                        Log.d("RC2Response", jsonData.toString());
+                                        String code = jsonData.getString("rc_twoId");
+                                        String name = jsonData.getString("rootCauseTwo");
+                                        rc2Code.add(code);
+                                        rc2Name.add(name);
+                                    }
+                                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, rc2Name);
+                                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                                    rc2.setAdapter(adapter);
+                                }
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(retrofit2.Call<JsonElement> call, Throwable t) {
+                Log.e("RetroError", t.toString());
+            }
+        });
+    }
+
+    private void getRC3(String rc2Code) {
+        String authKey = "ac7b51de9d888e1458dd53d8aJAN3ba6f";
+        String action = "getRCthree";
+        String RCtwoId = rc2Code;
+
+        RCRequest rcRequest = new RCRequest();
+        rcRequest.setAuthkey(authKey);
+        rcRequest.setAction(action);
+        rcRequest.setRC2(RCtwoId);
+
+        RCInterface apiService = ApiClient.getClient().create(RCInterface.class);
+        Call<JsonElement> call = apiService.getRCDetail(rcRequest);
+        call.enqueue(new Callback<JsonElement>() {
+            @Override
+            public void onResponse(retrofit2.Call<JsonElement> call, Response<JsonElement> response) {
+                try {
+                    if (response.isSuccessful()) {
+                        JSONObject jsonObject = new JSONObject(String.valueOf(response.body()));
+                        status = jsonObject.getString("Status");
+                        if (status.equals("Failure")) {
+                            Log.d("Failure", "error");
+                        } else if (status.equals("Success")) {
+                            try {
+                                result = jsonObject.getJSONArray("data");
+                                if (result != null) {
+                                    rc3Code = new ArrayList<String>();
+                                    rc3Name = new ArrayList<String>();
+                                    for (int i = 0; i < result.length(); i++) {
+                                        JSONObject jsonData = new JSONObject(String.valueOf(result.getString(i)));
+                                        Log.d("RC3Response", jsonData.toString());
+                                        String code = jsonData.getString("rc_thirdId");
+                                        String name = jsonData.getString("rootCauseThree");
+                                        rc3Code.add(code);
+                                        rc3Name.add(name);
+                                    }
+                                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, rc3Name);
+                                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                                    rc3.setAdapter(adapter);
+                                }
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(retrofit2.Call<JsonElement> call, Throwable t) {
+                Log.e("RetroError", t.toString());
+            }
+        });
     }
 
     private void getAssignment(String srText) {
@@ -133,36 +297,6 @@ public class SRDetail extends Fragment {
                                     podName.setText(order.getPodName());
                                     devicePort.setText(order.getDeviceName() + " : " + order.getPortId());
                                     btnEndTime.setVisibility(View.GONE);
-                                    btnStartTime.setOnClickListener(new View.OnClickListener() {
-                                        @Override
-                                        public void onClick(View v) {
-                                            getLatLong(startLocation);
-                                            Calendar c = Calendar.getInstance();
-                                            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                                            String formattedDate = df.format(c.getTime());
-                                            startTime.setText(formattedDate);
-                                            btnStartTime.setVisibility(View.GONE);
-                                            btnEndTime.setVisibility(View.VISIBLE);
-                                        }
-                                    });
-                                    btnEndTime.setOnClickListener(new View.OnClickListener() {
-                                        @Override
-                                        public void onClick(View v) {
-                                            getLatLong(endLocation);
-                                            Calendar c = Calendar.getInstance();
-                                            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                                            String formattedDate = df.format(c.getTime());
-                                            endTime.setText(formattedDate);
-                                            btnEndTime.setVisibility(View.GONE);
-                                        }
-                                    });
-                                    customerMobile.setOnClickListener(new View.OnClickListener() {
-                                        @Override
-                                        public void onClick(View v) {
-                                            Uri number = Uri.parse("tel:" + customerMobile.getText().toString());
-                                            call_action(number);
-                                        }
-                                    });
                                 }
                             } catch (Exception e) {
                                 e.printStackTrace();
@@ -247,6 +381,66 @@ public class SRDetail extends Fragment {
         startLocation = (TextView) view.findViewById(R.id.startLocation);
         endLocation = (TextView) view.findViewById(R.id.endLocation);
         btnView = (Button) view.findViewById(R.id.btnView);
+        rc1 = (Spinner) view.findViewById(R.id.rc1);
+        rc2 = (Spinner) view.findViewById(R.id.rc2);
+        rc3 = (Spinner) view.findViewById(R.id.rc3);
+        getRC1();
+        rc1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                int itemPosition = rc1.getSelectedItemPosition();
+                String rc1Id = rc1Code.get(itemPosition).toString();
+                getRC2(rc1Id);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        rc2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                int itemPosition = rc2.getSelectedItemPosition();
+                String rc2Id = rc2Code.get(itemPosition).toString();
+                getRC3(rc2Id);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        btnStartTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getLatLong(startLocation);
+                Calendar c = Calendar.getInstance();
+                SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                String formattedDate = df.format(c.getTime());
+                startTime.setText(formattedDate);
+                btnStartTime.setVisibility(View.GONE);
+                btnEndTime.setVisibility(View.VISIBLE);
+            }
+        });
+        btnEndTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getLatLong(endLocation);
+                Calendar c = Calendar.getInstance();
+                SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                String formattedDate = df.format(c.getTime());
+                endTime.setText(formattedDate);
+                btnEndTime.setVisibility(View.GONE);
+            }
+        });
+        customerMobile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri number = Uri.parse("tel:" + customerMobile.getText().toString());
+                call_action(number);
+            }
+        });
         return view;
     }
 }
